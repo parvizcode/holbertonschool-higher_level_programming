6f3f6f0eba76f54315df9041ab1167e4cc5f8816
parvizcode <gparvizn@gmail.com>
2025-06-19 18:30:53 +0000

    Fixed syntax error in add_0.py and completed 0-add task

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+
+a = 1
+b = 2
+
 if __name__ == "__main__":
-    from add_0 import add
-
-    a = 1
-    b = 2
-
-    print(f"{a} + {b} = {add(a, b)}")
-<<<<<<< HEAD
-
-=======
->>>>>>> d9c144c (Fix path and add 0-add.py task)
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-<<<<<<< HEAD
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-=======
+    """My addition function"""
     return a + b
->>>>>>> d9c144c (Fix path and add 0-add.py task)
