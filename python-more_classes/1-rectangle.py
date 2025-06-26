4fc58f1aef45745d03fa134d44c29a83942e2af9
parvizcode <gparvizn@gmail.com>
2025-06-26 10:34:45 +0000

    feat: Implement Rectangle class with width and height properties

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,7 +7,6 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-        
         Args:
             width (int): The width of the rectangle (default 0)
             height (int): The height of the rectangle (default 0)
