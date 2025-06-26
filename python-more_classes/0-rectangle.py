24836f3d40f062aed42b07b6f7da2bb639c364b0
parvizcode <gparvizn@gmail.com>
2025-06-26 10:23:12 +0000

    Fixed: Empty Rectangle class implementation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an empty Rectangle class."""
 
+
 class Rectangle:
-    """Empty class that represents a rectangle."""
+    """Representation of a rectangle."""
     pass
