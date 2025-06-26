1d1f0ad336f2e21de0dd75d36054f980d737bead
parvizcode <gparvizn@gmai.com>
2025-06-26 15:13:32 +0000

    fix: PEP8 compliance - line length and spacing

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""This module provides a function to list an object's attributes and methods."""
+"""This module provides a function to list
+an object's attributes and methods."""
+
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object.
+    """Returns the list of available attributes
+    and methods of an object.
 
     Args:
         obj: The object to inspect
 
     Returns:
-        list: A list of strings representing the object's attributes and methods
+        list: A list of strings representing
+        the object's attributes and methods
     """
     return dir(obj)
