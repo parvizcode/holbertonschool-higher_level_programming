63db5f64980433a2335004c20cae4f418a88eb3e
parvizcode <gparvizn@gmail.com>
2025-06-27 11:37:29 +0000

    Add tests/1-my_list.txt and update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 
 
 class MyList(list):
-    """A subclass of list that adds a method to print the list sorted."""
+    """A subclass of list that prints its elements sorted."""
 
     def print_sorted(self):
         """Prints the list in ascending sorted order (without modifying the original)."""
---
+++ python-inheritance/tests/README.md
@@ -0,0 +1,1 @@
+# MyProject - A brief description of your project in one sentence.
