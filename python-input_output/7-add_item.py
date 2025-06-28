ba5e9f9a4855597cdd82b68afb61d194a4a7a8a7
parvizcode <gparvizn@gmail.com>
2025-06-28 11:42:22 +0000

    Add 7-add_item script to append command line args to JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Komut satırından verilen argümanları JSON dosyasına ekleyen script.
+Komut satırından alınan argümanları JSON listesine ekleyip kaydeden script.
 """
 
 import sys
@@ -9,6 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 
+
 def main():
     try:
         items = load_from_json_file(filename)
@@ -16,7 +17,6 @@ def main():
         items = []
 
     items.extend(sys.argv[1:])
-
     save_to_json_file(items, filename)
 
 
