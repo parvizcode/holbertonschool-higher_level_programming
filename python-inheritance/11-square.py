2da1fecab3cb1f13d1984eda681ea06b8e222d05
parvizcode <gparvizn@gmail.com>
2025-06-28 09:57:13 +0000

    Add Square class with str override returning [Square] width/height

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class that inherits from Rectangle.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
