a71167d98cb95c2e21f5cd37986b3831b371cb23
parvizcode <gparvizn@gmail.com>
2025-06-28 10:14:34 +0000

    Add module docstring to 101-add_attribute.py

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+"""Module for adding attributes to objects if possible."""
+
 def add_attribute(obj, name, value):
     """Adds a new attribute to an object if possible.
 
     Args:
         obj: The object to add an attribute to
-        name: Name of the new attribute
+        name (str): Name of the new attribute
         value: Value of the new attribute
 
     Raises:
         TypeError: If the object can't have new attributes
     """
-    if not hasattr(obj, '__dict__'):
+    if not hasattr(obj, '__dict__') and not hasattr(type(obj), '__slots__'):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
