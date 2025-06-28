834d1a472f5f65aeab80aaff56dab414f699b6a9
parvizcode <gparvizn@gmail.com>
2025-06-28 10:09:17 +0000

    Fix PEP8 line length violation in 101-add_attribute.py

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,21 +1,14 @@
-#!/usr/bin/python3
-"""
-This module defines a function that adds a new attribute to an object if possible.
-"""
-
-
-def add_attribute(obj, attr_name, attr_value):
-    """Add a new attribute to an object if possible.
+def add_attribute(obj, name, value):
+    """Nesneye yeni bir özellik ekler.
 
     Args:
-        obj: The object to which the attribute should be added.
-        attr_name: The name of the attribute.
-        attr_value: The value of the attribute.
+        obj: Özellik eklenecek nesne
+        name: Özelliğin adı
+        value: Özelliğin değeri
 
     Raises:
-        TypeError: If the object can't have new attributes.
+        TypeError: Nesneye yeni özellik eklenemezse
     """
-    if hasattr(obj, '__dict__'):
-        setattr(obj, attr_name, attr_value)
-    else:
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
