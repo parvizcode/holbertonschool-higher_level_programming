48b3009666c3ed5916e9367f9c8e5ef4cf1540ce
parvizcode <gparvizn@gmail.com>
2025-06-28 11:07:50 +0000

    Fix PEP8 issues in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""
-This module contains a function that writes a string to a UTF-8 text file
-and returns the number of characters written.
-"""
-
+"""Module for writing to files."""
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return number of characters written."""
-    with open(filename, "w", encoding="utf-8") as f:
+    """Writes a string to a text file (UTF8) and returns num of chars written.
+    
+    Args:
+        filename (str): Name of file to write to
+        text (str): Text to write to the file
+    
+    Returns:
+        int: Number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
