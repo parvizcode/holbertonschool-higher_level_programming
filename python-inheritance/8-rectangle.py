63ddcb325066ba14b3c2684943e2c9e3906ef57b
parvizcode <gparvizn@gmail.com>
2025-06-28 08:58:41 +0000

    Fix PEP8 style in Rectangle class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Defines a Rectangle class that inherits from BaseGeometry."""
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry."""
-    
+
     def __init__(self, width, height):
         """Initialize a new Rectangle.
         
