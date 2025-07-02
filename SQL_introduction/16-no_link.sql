da7f83e4702b07ca430359b18933cddf4a8d7ce5
parvizcode <gparvizn@gmail.com>
2025-07-02 14:51:10 +0000

    Add script to list records with non-empty name ordered by score descending

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- List records with non-empty name, showing score and name, ordered by score descending
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
