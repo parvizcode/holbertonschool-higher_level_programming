893b0aa591106b90b0f1b419090546a7d2a0903e
parvizcode <gparvizn@gmail.com>
2025-07-03 15:58:39 +0000

    Add 0-privileges.sql to list MySQL user privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,2 @@
--- List the privileges for user_0d_1 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List the privileges for user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
