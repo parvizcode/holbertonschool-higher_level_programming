cc981f129156e1d244121c5932696ae540cb85c1
parvizcode <gparvizn@gmail.com>
2025-07-03 15:41:52 +0000

    List MySQL user privileges for user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,16 @@
--- List the privileges of user_0d_1 on localhost
+-- Kullanıcı kontrolü yapalım
+SELECT IFNULL(
+  (SELECT CONCAT('User user_0d_1@localhost exists. Privileges: ') FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'),
+  'User user_0d_1@localhost does not exist'
+) AS Status;
+
+-- Sadece kullanıcı varsa yetkileri göster
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List the privileges of user_0d_2 on localhost
+-- Aynısını user_0d_2 için yapalım
+SELECT IFNULL(
+  (SELECT CONCAT('User user_0d_2@localhost exists. Privileges: ') FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'),
+  'User user_0d_2@localhost does not exist'
+) AS Status;
+
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
