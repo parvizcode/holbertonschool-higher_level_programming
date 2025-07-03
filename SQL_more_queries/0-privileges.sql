c7ff41c210c9fed65aa34d47a671b18a88f1c6c3
parvizcode <gparvizn@gmail.com>
2025-07-03 15:07:59 +0000

    Task 0: List MySQL user privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List the privileges of user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List the privileges of user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
