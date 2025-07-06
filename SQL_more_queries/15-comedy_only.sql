e6aa5b713af4ae4e1a998a90f8e8ee8621fe9b74
parvizcode <gparvizn@gmail.com>
2025-07-06 17:27:15 +0000

    Add SQL script that lists all Comedy shows sorted by title

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,10 @@
--- Dexter
-SELECT tv_genres.name
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- Each record displays: tv_shows.title
+-- Results sorted in ascending order by the show title
+
+SELECT tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+  AND tv_show_genres.genre_id = tv_genres.id
+  AND tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title;
