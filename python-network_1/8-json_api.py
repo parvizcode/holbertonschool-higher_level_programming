bc43cb89b2ce17b1aa0af78cd8549c98efd84cf3
parvizcode <gparvizn@gmail.com>
2025-07-13 00:49:23 +0000

    Add script to POST letter to search_user and display results

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """Sends POST request to search_user API and processes the JSON response"""
 
+
 import sys
 import requests
+
 
 def main():
     q = sys.argv[1] if len(sys.argv) > 1 else ""
 
     try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-        response.raise_for_status()  # HTTP hataları için
+        response = requests.post(
+            "http://0.0.0.0:5000/search_user", data={"q": q}
+        )
+        response.raise_for_status()  # HTTP errors
 
         try:
             data = response.json()
@@ -22,6 +26,7 @@ def main():
 
     except requests.RequestException:
         print("Not a valid JSON")
+
 
 if __name__ == "__main__":
     main()
